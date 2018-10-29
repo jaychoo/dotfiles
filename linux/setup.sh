e127efb506098480092d07dbfa01f9c1f45988af
jaychoo <jaychoo@gmail.com>
2018-10-29 14:08:04 -0400

    add linux setup

---
+++ linux/setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# init
+sudo add-apt-repository ppa:gerardpuig/ppa
+sudo apt-get update
+sudo apt-get install build-essential curl wget git ibus-hangul emacs-nox geary neofetch -y
+
+
+# ohmybash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# i3
+sudo apt-get install i3-wm dunst i3lock i3status suckless-tools -y
+sudo apt-get install compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior -y
+
+# pyenv/pipenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+pyenv update
+
+sudo apt install python3-pip python3-dev -y
+pip3 install --user pipenv
+echo "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+
+# nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+nvm update && nvm install node
+npm install -g @vue/cli
+
+# applications
+sudo apt-get install vlc -y
+
+# development tools
+git clone git@github.com:jaychoo/emacs.d.git ~/.emacs.d
+
+sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+sudo apt-get update
+sudo apt-get install spotify-client
+
+sudo apt install apt-transport-https dirmngr -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
+sudo apt update && sudo apt install monodevelop
+
+sudo snap install pycharm-community --classic
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update && sudo apt install mono-devel
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update
+sudo apt-get install code
+# reload bashrc
+source ~/.bashrc
